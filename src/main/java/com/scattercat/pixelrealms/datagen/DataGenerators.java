7880c9321deba94a847fb6934c83d2d9b0308fbe
scattercat_123 <replyatharv@gmail.com>
2025-01-23 18:09:44 +0530

    all datagen custom blockstates fixed..

--- src/main/java/com/scattercat/pixelrealms/datagen/DataGenerators.java
+++ src/main/java/com/scattercat/pixelrealms/datagen/DataGenerators.java
@@ -5,6 +5,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -25,8 +26,10 @@ public class DataGenerators {
 
         generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(),
                 List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, lookupProvider));
 
-
+        BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
+        generator.addProvider(event.includeServer(), blockTagsProvider);
 
         generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModBlocksStateProvider(packOutput, existingFileHelper));
--- src/main/java/com/scattercat/pixelrealms/datagen/ModBlockTagProvider.java
+++ src/main/java/com/scattercat/pixelrealms/datagen/ModBlockTagProvider.java
@@ -1,4 +1,37 @@
 package com.scattercat.pixelrealms.datagen;
+import com.scattercat.pixelrealms.PixelRealms;
+import com.scattercat.pixelrealms.block.ModBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
 
-public class ModBlockTagProvider {
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends BlockTagsProvider {
+    public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, PixelRealms.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.LIGHT_BLOCK.get())
+                .add(ModBlocks.RAW_LIGHT_BLOCK.get())
+                .add(ModBlocks.LIGHT_ORE.get())
+                .add(ModBlocks.LIGHT_DEEPSLATE_ORE.get())
+                .add(ModBlocks.CHISELED_STAIRS.get());
+
+        tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.LIGHT_DEEPSLATE_ORE.get())
+                .add(ModBlocks.RAW_LIGHT_BLOCK.get());
+
+        tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.LIGHT_BLOCK.get());
+
+        tag(BlockTags.NEEDS_STONE_TOOL)
+                .add(ModBlocks.SOLAR_PANEL.get());
+    }
 }
